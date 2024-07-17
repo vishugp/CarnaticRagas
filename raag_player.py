6c8f60a7fc6f854e4e3e61e283526056ff220291
Vishwanath Guruvayur <vishvesh2704@gmail.com>
2024-07-16 20:07:58 -0400

    Changed notation to single letter

--- raag_player.py
+++ raag_player.py
@@ -10,18 +10,18 @@ class RaagPlayer:
   DURATION = 2
   base = 200
 
-  notes_list = {'Sa':1, 
-                'Ri1':16/15,
-                'Ri2':9/8,
-                'Ga1':6/5,
-                'Ga2':81/64,
-                'Ma1':4/3,
-                'Ma2':45/32,
-                'Pa':3/2,
-                'Da1':8/5,
-                'Da2':27/16,
-                'Ni1':16/9,
-                'Ni2':15/8 
+  notes_list = {'S':1, 
+                'R1':16/15,
+                'R2':9/8,
+                'G1':6/5,
+                'G2':81/64,
+                'M1':4/3,
+                'M2':45/32,
+                'P':3/2,
+                'D1':8/5,
+                'D2':27/16,
+                'N1':16/9,
+                'N2':15/8 
 }
   
   def __init__(self, play_notes):
@@ -31,7 +31,7 @@ class RaagPlayer:
 
   def aaro_avro(self, raag):
     freq_tuple = [(n,self.notes_list[n]) for n in play_notes]
-    return [(note,self.base*v) for note,v in freq_tuple + [('Sa2',2),('   ',0),('Sa2',2)] + freq_tuple[::-1]]
+    return [(note,self.base*v) for note,v in freq_tuple + [('S2',2),('   ',0),('S2',2)] + freq_tuple[::-1]]
 
   def generate_note(self, notes):
     
@@ -56,9 +56,7 @@ class RaagPlayer:
 
 if __name__=='__main__':
 
-  play_notes = ['Sa','Ri2','Ga2','Ma1','Pa','Da2','Ni2']
-
-
+  play_notes = ['S','R2','G2','M1','P','D2','N2']
   a = RaagPlayer(play_notes)
 
 
