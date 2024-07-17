cdc629b59fde89881f23dd9da2d48addbe173f20
Vishwanath Guruvayur <vishvesh2704@gmail.com>
2024-07-16 20:32:06 -0400

    Cretaed a raagam player based on database of janya raagams

--- raag_player.py
+++ raag_player.py
@@ -7,30 +7,34 @@ class RaagPlayer:
   FORMAT = pyaudio.paInt16
   CHANNELS = 2
   RATE = 44100 
-  DURATION = 2
+  DURATION = 1.5
   base = 200
 
   notes_list = {'S':1, 
                 'R1':16/15,
                 'R2':9/8,
+                'R3':1.186,
                 'G1':6/5,
                 'G2':81/64,
+                'G3':1.321,
                 'M1':4/3,
                 'M2':45/32,
                 'P':3/2,
                 'D1':8/5,
                 'D2':27/16,
+                'D3':1.69,
                 'N1':16/9,
-                'N2':15/8 
+                'N2':15/8,
+                'N3':1.9
 }
   
   def __init__(self, play_notes):
-    print("Hi")
+    print(play_notes)
     self.generate_note(self.aaro_avro(play_notes))
 
 
   def aaro_avro(self, raag):
-    freq_tuple = [(n,self.notes_list[n]) for n in play_notes]
+    freq_tuple = [(n,self.notes_list[n]) for n in raag]
     return [(note,self.base*v) for note,v in freq_tuple + [('S2',2),('   ',0),('S2',2)] + freq_tuple[::-1]]
 
   def generate_note(self, notes):
